9be30dafd773f21331bb4010853e394eea5eaf5a
Harold Onyebuchi Ekemkpa <haroldonyebuchi507@gmail.com>
2026-07-26 12:18:33 +0100

    auth error fixed

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -21,6 +21,7 @@ export const googleOAuthScopes = [
   "openid",
   "email",
   "profile",
+  "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/gmail.send",
   "https://www.googleapis.com/auth/calendar.events",
   "https://www.googleapis.com/auth/spreadsheets",
